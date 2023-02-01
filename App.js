e6f3f33642366f7d09a5ad940b95cfa7294b09fa
Miko <miko.prykari@gmail.com>
2023-02-01 15:31:38 +0200

    Added basic components and testing passing down params

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, StyleSheet, Button, TextInput, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DetailsScreen from './components/DetailsScreen';
+
+function HomeScreen({ navigation }) {
+  const [user, OnChangeUser] = React.useState('Username')
+  const [pwd, OnChangePwd] = React.useState('Password')
+
+  const login = () => {
+    if(user !== "Username" && pwd !== 'Password' && user !== '' && pwd !== ''){
+      navigation.navigate('Details', {user: user, pwd: pwd,})
+      OnChangeUser('Username')
+      OnChangePwd('Password')
+    }else{
+      Alert.alert("Please fill all fields")
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TextInput onChangeText={OnChangeUser}
+      style={styles.input}
+      value={user}
+      onFocus= {() => {if(user=='Username'){OnChangeUser('')}}}
+      />
+      <TextInput onChangeText={OnChangePwd}
+      style={styles.input}
+      value={pwd}
+      onFocus= {() => {if(pwd=='Password'){OnChangePwd('')}}}
+      />
+      <Button
+        title="Log in"
+        onPress={() => login()}
+      />
     </View>
+  );
+  
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}
+        options={{headerShown: false}}
+       />
+        <Stack.Screen name="Details" component={DetailsScreen} 
+        options={{headerBackVisible: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    width: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
+
+export default App;
